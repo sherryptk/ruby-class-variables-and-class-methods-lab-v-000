29614ac994671bb7f3c7195ddcf4e5f7e9e3006f
Sherry El Garhy <sherryptk@gmail.com>
2017-11-15 00:35:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,12 @@ class Song
 
   @@count = 0
   @@artists = []
-  @@genre = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
-    @genre = genre
+    @genres = genre
     @@count += 1
     @@artists << artist
     @@genres << genre
