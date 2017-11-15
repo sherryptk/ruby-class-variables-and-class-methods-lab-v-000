e05b895368d9ff8be1ef505fe2d1df3659cb9569
Sherry El Garhy <sherryptk@gmail.com>
2017-11-15 00:39:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   attr_reader :name, :artist, :genre
-  
+
 
   @@count = 0
   @@artists = []
@@ -15,7 +15,7 @@ class Song
     @@genres << genre
   end
 
-  def genre 
+  def genre
     @genre
   end
 end
