72af31f26455bc9afd032bb56cc801663558da5d
Sherry El Garhy <sherryptk@gmail.com>
2017-11-15 00:47:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   end
 
   def self.genres
-    @@genres
+    @@genres.uniq
   end
 
   def self.count
@@ -24,7 +24,7 @@ class Song
   end
 
   def self.artists
-    @@artists
+    @@artists.uniq
   end
 
 end
